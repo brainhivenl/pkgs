3bff5ee30e8ac6969e97b56ee907042bfd59429b
Julia Mertz <info@juliamertz.dev>
2025-05-14 10:31:15 +0200

    fix: devenv apple-sdk
    amend

--- pkgs/apple-sdk.nix
+++ pkgs/apple-sdk.nix
@@ -1,29 +1,21 @@
 {
   lib,
   darwin,
-  symlinkJoin,
   apple-sdk_15,
   package ? apple-sdk_15,
   extraLibraries ? [],
 }:
-symlinkJoin {
-  inherit (package) pname version meta;
-
-  paths = [package];
-
-  buildInputs = [darwin.libtapi];
-
-  postBuild = let
+package.overrideAttrs {
+  postInstall = let
     mkStub = dylib:
     # sh
     ''
-      tapi stubify "${dylib}/lib/${dylib.pname}.dylib" -o "$library_path/${dylib.pname}.tbd"
+      ${lib.getExe darwin.libtapi} stubify "${dylib}/lib/${dylib.pname}.dylib" -o "$library_path/${dylib.pname}.tbd"
     '';
   in
     # sh
     ''
       library_path="$out/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${package.version}.sdk/usr/lib"
-      mkdir -p $library_path
       ${lib.concatStringsSep "\n" (map mkStub extraLibraries)}
     '';
 }
