8b80d666a94bddc04f337b388aeae308b3490c29
Julia Mertz <info@juliamertz.dev>
2025-09-08 10:02:59 +0200

    chore: bump thalassa cloud cli

--- pkgs/thalassa-cloud-cli.nix
+++ pkgs/thalassa-cloud-cli.nix
@@ -4,16 +4,16 @@
 }:
 buildGoModule {
   pname = "thalassa-cloud-cli";
-  version = "0.4.1";
+  version = "0.6.0";
 
   src = fetchFromGitHub {
     owner = "thalassa-cloud";
     repo = "cli";
-    rev = "9250bc8bf294446f6519c5147aad466a3c4a6a4f";
-    hash = "sha256-rsimcFG2uwwpsJEUpJuP7CA5AwyITFV65yCg75fcisU=";
+    rev = "9db1838d59257de08c17623dc9c76989b7022fcd";
+    hash = "sha256-cubphVGdyu0Y2SlyVS+lzaOfAwGBcYVxJw7NK1d3/0o=";
   };
 
-  vendorHash = "sha256-e9z5QGlbJ0LM1z8AUNzjGiOSkPExBQHdwJWdGxv/Zxc=";
+  vendorHash = "sha256-X43mgoO+ukIZd2y93csaD4DP20n3llOG7U9IvZl3FTw=";
 
   postInstall = ''
     mv $out/bin/cli $out/bin/tcloud
